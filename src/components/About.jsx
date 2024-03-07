2a95f7a9edf04aa9a80af64adb87790e06aca72c
Ankita Jha <ankitajha@Ankitas-MacBook-Air.local>
2024-03-07 16:42:17 -0500

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
 
       <motion.p
@@ -48,15 +48,11 @@ const About = () => {
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
         
-        I'm a software developer with a diverse skillset and a passion for creating innovative solutions. Whether it's developing a new application from scratch or improving an existing system, I enjoy the challenge of finding elegant and efficient solutions to complex problems.
+        
+I'm a software engineer with a Master's in Software Engineering from Northeastern University. Experienced in developing robust applications and systems, with expertise in Java, Python, and cloud technologies like AWS. 
+Interned at Amazon, contributing to real-time monitoring systems for Amazon Robotics. Skilled in full-stack development, with a focus on creating impactful solutions. Ready to tackle new challenges and contribute to innovative projects.
 
-My experience ranges from front-end development with HTML, CSS, and JavaScript to back-end development with languages like Java and Python. I'm comfortable working with databases and APIs, and have experience with cloud-based technologies such as AWS and Google Cloud.
 
-One of my favorite aspects of software development is the constant learning and exploration that comes with the field. I'm always eager to explore new technologies and techniques, and am constantly looking for ways to improve my skills and stay up-to-date with the latest trends.
-
-Outside of work, I enjoy hiking and photography. I find that getting out into nature helps me clear my mind and approach problems from new angles. I'm also an avid reader, and enjoy books on topics ranging from software development to history and philosophy.
-
-Overall, I'm excited to continue growing as a software developer and working on challenging projects that make a positive impact. Whether it's creating new applications or improving existing ones, I'm always striving to find the most efficient and effective solutions.
 
 
 
